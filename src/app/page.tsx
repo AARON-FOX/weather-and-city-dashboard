532f1b218c70702268e0a2875809e0e44bb6dac9
Artem Vikuliev <ffox2645@gmail.com>
2026-02-20 14:06:02 +0200

    feat: test version of data retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '@/store';
+import { fetchWeather } from '@/store/slices/weatherSlice';
+
 export default function Home() {
-  return <></>;
+  const dispatch = useDispatch<AppDispatch>();
+  const { currentWeather, isLoading, error } = useSelector((state: RootState) => state.weather);
+
+  useEffect(() => {
+    dispatch(fetchWeather('Kyiv'));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div className="container">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="container">Error: {error}</div>;
+  }
+
+  return (
+    <div className="container">
+      {currentWeather && (
+        <section style={{ marginTop: '20px' }}>
+          <h1>{currentWeather.name}</h1>
+          <p style={{ fontSize: '24px' }}>
+            {Math.round(currentWeather.main.temp)}°C
+          </p>
+          <p>{currentWeather.weather[0].description}</p>
+        </section>
+      )}
+    </div>
+  );
 }
