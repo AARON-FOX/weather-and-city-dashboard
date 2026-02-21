6001a0448ef681e0e77c25c7847d08d8fd8bf0d6
Artem Vikuliev <ffox2645@gmail.com>
2026-02-21 17:00:41 +0200

    feat: redux persist added and managed

--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useRef } from 'react';
 import { Provider } from 'react-redux';
-import { makeStore, AppStore } from './index';
+import { persistor, store } from '.';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeManager } from '@/components/utils/ThemeManager';
 
 export default function StoreProvider({
@@ -10,13 +10,12 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore | null>(null);
-
-  // eslint-disable-next-line react-hooks/refs
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
-
-  // eslint-disable-next-line react-hooks/refs
-  return <Provider store={storeRef.current}><ThemeManager /> {children} </Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeManager />
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import settingsReduces from './slices/settingsSlice';
 import weatherReducer from './slices/weatherSlice';
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      settings: settingsReduces,
-      weather: weatherReducer,
-    },
-  });
+const rootReducer = combineReducers({
+  settings: settingsReduces,
+  weather: weatherReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['settings'],
 };
 
-export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const makeStore = () => {
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, REGISTER, PURGE, REGISTER],
+        },
+      }),
+  });
+
+  return store;
+};
+
+export const store = makeStore();
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
